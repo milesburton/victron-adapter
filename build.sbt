cd334676faa784a4f5c332a48a345ac1b3ee54e8
Miles Burton <miles@mnetcs.com>
2015-08-27 06:18:41 +0100

    Temp commit

---
+++ build.sbt
@@ -0,0 +1,43 @@
+name := "solar"
+
+mainClass in Compile := Some("Example.Class")
+
+organization := "com.mba"
+
+scalaVersion := "2.11.2"
+
+version := "0.0.1"
+
+licenses := Seq("The Apache Software License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"))
+
+homepage := Some(url("https://blog.milesburton.com"))
+
+resolvers += "Sonatype OSS Snapshots" at
+  "https://oss.sonatype.org/content/repositories/snapshots"
+
+
+libraryDependencies += "ch.inventsoft.akka" %% "rxtx-akka-io" % "1.0.4"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.4"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % "2.3.4" % "test"
+
+libraryDependencies += "org.rxtx" % "rxtx" % "2.1.7"
+
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+
+pomIncludeRepository := { _ => false}
+
+pomExtra := (
+  <scm>
+    <url>git@github.com:msiegenthaler/rxtx-akka-io</url>
+    <connection>scm:git:git@github.com:msiegenthaler/rxtx-akka-io</connection>
+  </scm>
+    <developers>
+      <developer>
+        <id>msiegenthaler</id>
+        <name>Mario Siegenthaler</name>
+        <url>https://github.com/msiegenthaler</url>
+      </developer>
+    </developers>)
---
+++ src/main/scala/Example.scala
@@ -0,0 +1,64 @@
+
+import akka.actor.{ Actor, ActorRef, Props, ActorSystem }
+import akka.io.IO
+import akka.util.ByteString
+import akka.actor.Stash
+import rxtxio.Serial
+import rxtxio.Serial._
+
+class Example(port: String) extends Actor with Stash {
+  import context.system
+
+  override def preStart = {
+    IO(Serial) ! Open(port, 19200)
+  }
+
+  override def postStop = {
+    println("Stopped")
+    system.shutdown
+  }
+
+  override def receive = {
+    case Opened(operator, _) =>
+      println("Connected to port")
+      context become open(operator)
+      unstashAll()
+    case CommandFailed(_, error) =>
+      println(s"Could not connect to port: $error")
+      context stop self
+    case other => stash()
+  }
+
+  def open(operator: ActorRef): Receive = {
+    case "close" =>
+      println("Closing")
+      operator ! Close
+
+    case s: String => //external input
+      operator ! Write(ByteString(s))
+
+    case Received(data) =>
+      print("Received data from serial port: ")
+      println(data.decodeString("UTF-8"))
+
+    case Closed =>
+      println("Serial port closed")
+      context stop self
+  }
+}
+
+object Example extends App {
+  val port = "/dev/cu.usbserial-A6008hNp"
+
+  val system = ActorSystem("Example")
+  val actor = system.actorOf(Props(new Example(port)), "e1")
+
+  //The following lines are just for the sake of an example, never program in
+  // akka like that.. normally your logic would be embedded in the example
+  // actor
+  actor ! "Hi from the computer"
+  Thread.sleep(2000)
+  actor ! "close"
+
+  system.awaitTermination
+}
